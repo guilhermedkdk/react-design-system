99534dfefa3a282e2df9eadb6e561893b21751ff
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-03-04 20:30:28 -0300

    feat: add tokens mdx documentation

---
+++ packages/docs/.storybook/preview.ts
@@ -0,0 +1,12 @@
+import { Preview } from '@storybook/react';
+import { themes } from '@storybook/theming';
+ 
+const preview: Preview = {
+  parameters: {
+    docs: {
+      theme: themes.dark,
+    },
+  },
+};
+ 
+export default preview;
