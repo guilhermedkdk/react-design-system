d4afd20af9e8360f3c4607b5e1de0449a7c92d60
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-03-05 00:25:11 -0300

    feat: add box component

---
+++ packages/react/src/components/Box.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const Box = styled("div", {
+  padding: "$6",
+  border: "1px solid $gray600",
+  borderRadius: "$md",
+
+  backgroundColor: "$gray800",
+});
+
+export interface BoxProps extends ComponentProps<typeof Box> {}
