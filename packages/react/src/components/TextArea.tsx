e7ce7f89295f01e4a6a7bbc1a9975369c856f7ca
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-03-05 02:00:56 -0300

    feat: add textarea component

---
+++ packages/react/src/components/TextArea.tsx
@@ -0,0 +1,35 @@
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const TextArea = styled('textarea', {
+  boxSizing: 'border-box',
+  minHeight: 80,
+  padding: '$3 $4',
+
+  border: '2px solid $gray900',
+  borderRadius: '$sm',
+  
+  resize: 'vertical',
+  fontFamily: '$default',
+  fontSize: '$sm',
+  fontWeight: '$regular',
+  color: '$white',
+  
+  backgroundColor: '$gray900',
+
+  '&:focus': {
+    outline: 0,
+    borderColor: '$ignite300',
+  },
+
+  '&:disabled': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
+
+export interface TextAreaProps extends ComponentProps<typeof TextArea> {}
